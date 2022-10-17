0a118ccf55a78e4a2fee31f22638e25986426ef3
Dmytro Botnarenko <dmytro.botnarenko@gmail.com>
2022-10-17 14:22:06 +0300

    Add Enemy position and update code

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +17,10 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
+    if (this.x > 500) {
+        this.x = -90
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -45,14 +52,12 @@ Player.prototype.handleInput = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-const enemy1 = new Enemy();
+const enemy1 = new Enemy(-90, 60, 100);
 const allEnemies = [
     enemy1
 ];
 
-const player = new Player();
-player.x = 200;
-player.y = 420;
+const player = new Player(200, 420);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
